fe8e65259da39938f561aa554879c6fda9ac90d0
Orange <osdev123@proton.me>
2023-08-02 21:22:34 +0100

    Added first components

---
+++ webapp/src/components/AuctionableErc20Item.tsx
@@ -0,0 +1,191 @@
+import React, {useEffect, useState } from 'react';
+import { ERC20ABI as erc20Abi} from 'abi/ERC20ABI'
+import { AuctionCoordinator as auctionCoordinatorAbi} from 'abi/AuctionCoordinator'
+import {ethers} from 'ethers'
+import {Button} from '@chakra-ui/react'
+import { Stack } from '@chakra-ui/react'
+import { Divider } from '@chakra-ui/react'
+import {
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+    PopoverBody,
+    PopoverFooter,
+    Image,
+  } from '@chakra-ui/react'
+import {
+    NumberInput,
+    NumberInputField,
+    NumberInputStepper,
+    NumberIncrementStepper,
+    NumberDecrementStepper,
+  } from '@chakra-ui/react'
+
+import { Center } from '@chakra-ui/react'
+import { Heading, Box } from "@chakra-ui/layout"
+
+interface Props {
+    addressContract: string,
+    iteration: number,
+    auctionCoordinatorContract: string,
+    connectedAccount: string | undefined,
+    balance: number,
+    formattedBalance: number,
+    orangeStandTicketAddress: string,
+    provider: ethers.providers.Web3Provider | undefined,
+}
+
+declare let window: any;
+
+export default function AuctionableErc20Item(props:Props){
+    const addressContract = props.addressContract
+    const connectedAccount = props.connectedAccount;
+    const auctionCoordinatorContract = props.auctionCoordinatorContract
+
+    const [balance, setBalance]=useState<number>(0);
+    const [symbol, setSymbol]=useState<string>();
+    const [decimalPlaces, setDecimalPlaces]=useState<number>(0);
+
+    const [initialPrice, setInitialPrice]=useState<number>(15);
+    const [increaseValue, setIncreaseValue]=useState<number>(1);
+    const [displayAmountValue, setDisplayAmountValue] = React.useState(0)
+    const [amountValue, setAmountValue] = React.useState(0)
+    const handleChange = (value) => setDisplayAmountValue(value)
+    const [costValue, setCostValue]=useState<number>(1);
+
+    //call when currentAccount change
+    useEffect(()=>{
+      if(connectedAccount == undefined) return
+      const erc20 = new ethers.Contract(addressContract, erc20Abi, props.provider!)
+
+      const createItem = async () => {
+        await createErc20AuctionItem(erc20, connectedAccount);
+      }
+      createItem();
+      
+    }, [addressContract,connectedAccount])
+
+    async function createErc20AuctionItem(erc20: ethers.Contract, account: string | undefined){
+      let bal = await erc20.balanceOf(account);
+      let sym = await erc20.symbol();
+      let decPlaces = await erc20.decimals();
+      setSymbol(sym);
+      setBalance(bal / (10 ** decPlaces));
+      setDecimalPlaces(decPlaces);
+      setAmountValue(+bal);
+      setDisplayAmountValue(bal / (10 ** decPlaces));
+    }
+  
+    async function createAuction(){
+      let rawAmountValue = +displayAmountValue * (10 ** decimalPlaces);
+      const signer = props.provider!.getSigner()
+
+      const auctionCoordinator = new ethers.Contract(auctionCoordinatorContract, auctionCoordinatorAbi, signer);
+      const erc20 = new ethers.Contract(addressContract, erc20Abi, signer);
+
+      await erc20.transfer(auctionCoordinatorContract, rawAmountValue.toString());
+      await auctionCoordinator.createErc20Auction(erc20.address, rawAmountValue.toString(), connectedAccount, increaseValue, 
+        initialPrice, props.orangeStandTicketAddress, ethers.utils.parseUnits(costValue.toString(), "ether"));
+    }
+
+    return (
+      <Box  mb={0} p={4} w='100%' borderWidth="2px" borderRadius="lg" borderColor={'#a3c139'}>
+            <Center>
+              <Image
+                objectFit='cover'
+                src={symbol + '.png'}
+                alt={symbol}
+                width='322px'
+                height='322px'
+              />
+            </Center>
+            <Center>
+              <Heading my={4}  fontSize='xl'>{props.formattedBalance} {symbol}</Heading>
+            </Center>
+            <Stack
+              divider={<Divider borderColor='gray.200' />}
+              spacing={4}
+              align='stretch'>
+              <Popover>
+                  <PopoverTrigger>
+                      <Button type="button" w='100%' style={{
+                          backgroundColor: '#a3c139'
+                        }}>
+                          Create Auction...
+                      </Button>
+                  </PopoverTrigger>
+                  <PopoverContent bg={'#fef1df;'}>
+                      <PopoverBody border='0'
+                          display='flex'
+                          alignItems='center'
+                          justifyContent='space-between'
+                          pb={4}>
+                          <Box>Amount:</Box>
+                          <NumberInput maxW='100px' mr='2rem' value={displayAmountValue} step={balance/10} min={0} max={balance} onChange={handleChange}>
+                              <NumberInputField />
+                              <NumberInputStepper>
+                              <NumberIncrementStepper />
+                              <NumberDecrementStepper />
+                              </NumberInputStepper>
+                          </NumberInput>
+                      </PopoverBody>
+                      <PopoverBody border='0'
+                          display='flex'
+                          alignItems='center'
+                          justifyContent='space-between'
+                          pb={4}>
+                          <Box>Initial price:</Box>
+                          <NumberInput maxW='100px' mr='2rem' defaultValue={initialPrice} onChange={(val) => setInitialPrice(+val)}>
+                              <NumberInputField />
+                              <NumberInputStepper>
+                              <NumberIncrementStepper />
+                              <NumberDecrementStepper />
+                              </NumberInputStepper>
+                          </NumberInput>
+                      </PopoverBody>
+                      <PopoverBody border='0'
+                          display='flex'
+                          alignItems='center'
+                          justifyContent='space-between'
+                          pb={4}>
+                          <Box>Bidding period in minutes:</Box>
+                          <NumberInput maxW='100px' mr='2rem' defaultValue={increaseValue} onChange={(val) => setIncreaseValue(+val)}>
+                              <NumberInputField />
+                              <NumberInputStepper>
+                              <NumberIncrementStepper />
+                              <NumberDecrementStepper />
+                              </NumberInputStepper>
+                          </NumberInput>
+                      </PopoverBody>
+                      <PopoverBody border='0'
+                          display='flex'
+                          alignItems='center'
+                          justifyContent='space-between'
+                          pb={4}>
+                          <Box>Bid cost:</Box>
+                          <NumberInput maxW='100px' mr='2rem' defaultValue={costValue} onChange={(val) => setCostValue(+val)}>
+                              <NumberInputField />
+                              <NumberInputStepper>
+                              <NumberIncrementStepper />
+                              <NumberDecrementStepper />
+                              </NumberInputStepper>
+                          </NumberInput>
+                      </PopoverBody>
+                      <PopoverBody border='0'
+                          display='flex'
+                          alignItems='center'
+                          alignContent={'right'}
+                          justifyContent='flex-end'
+                          pb={4}>
+                            <Button colorScheme='blue' onClick={createAuction} style={{
+                  backgroundColor: '#a3c139',
+                  color: 'black'
+                }}>Start auction</Button>
+                      </PopoverBody>
+                      <PopoverFooter>All auctions will have a 10 minute bidding period</PopoverFooter>
+                  </PopoverContent>
+              </Popover>
+            </Stack>
+          </Box>
+    )
+  }
---
+++ webapp/src/components/AuctionableErc721Item.tsx
@@ -0,0 +1,176 @@
+import React, {useEffect, useState } from 'react';
+import { ERC721ABI as erc721Abi} from 'abi/ERC721ABI'
+import { AuctionCoordinator as auctionCoordinatorAbi} from 'abi/AuctionCoordinator'
+import {ethers} from 'ethers'
+import {
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+    PopoverBody,
+    PopoverFooter,
+    NumberInput,
+    NumberInputField,
+    NumberInputStepper,
+    NumberIncrementStepper,
+    NumberDecrementStepper,
+    Center,
+    useToast,
+    Button,
+    Image,
+    Stack,
+    Divider,
+  } from '@chakra-ui/react'
+import { Heading, Box } from "@chakra-ui/layout"
+
+interface Props {
+    addressContract: string;
+    iteration: number;
+    auctionCoordinatorContract: string;
+    orangeStandTicketAddress: string;
+}
+
+declare let window: any;
+
+export default function AuctionableErc721Item(props:Props){
+    const addressContract = props.addressContract
+    const iteration = props.iteration
+    const auctionCoordinatorContract = props.auctionCoordinatorContract
+    const [url,setUrl]=useState<string>()
+    const [number,setNumber]=useState<number>()
+
+    const [initialValue, setInitialValue]=useState<number>(15);
+    const [increaseValue, setIncreaseValue]=useState<number>(1);
+    const [costValue, setCostValue]=useState<number>(1);
+    const [buttonDisabled, setButtonDisabled]=useState<boolean>(false);
+
+    const toast = useToast()
+
+    //call when currentAccount change
+    useEffect(()=>{
+      if(addressContract == undefined) return
+  
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const erc721 = new ethers.Contract(addressContract, erc721Abi, provider)
+
+      const createItem = async () => {
+        const {tokenId, imageUri } = await createErc721AuctionItem(erc721, iteration);
+        setNumber(tokenId);
+        setUrl(imageUri);
+      }
+      createItem();
+      return () => {}
+    }, [addressContract,iteration])
+
+    async function createErc721AuctionItem(erc721: ethers.Contract, tokenId: number){
+      let uri = await erc721.tokenURI(tokenId);
+      var res = await fetch(uri);
+      var json = await res.json();
+      return {
+        tokenId: +tokenId,
+        imageUri: 'https://ipfs.io/ipfs/' + json.image.replace(/ipfs:\/\//g, '')
+      }
+    }
+  
+    async function createAuction(){
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const signer = provider.getSigner()
+
+      const auctionCoordinator = new ethers.Contract(auctionCoordinatorContract, auctionCoordinatorAbi, signer);
+      const erc721 = new ethers.Contract(addressContract, erc721Abi, signer);
+
+      //await erc721.transferFrom(globalThis.connectedAddress, auctionCoordinatorContract, iteration);
+      await erc721.approve(auctionCoordinatorContract, iteration);
+      await auctionCoordinator.createAuction(erc721.address, iteration, 
+        globalThis.connectedAddress, increaseValue, 
+        initialValue, 
+        props.orangeStandTicketAddress,
+        ethers.utils.parseUnits(costValue.toString(), "ether"));
+    }
+  
+    return (
+      <Box  mb={0} p={4} w='100%' borderWidth="2px" borderRadius="lg" borderColor={'#a3c139'}>
+            <Center>
+              <Image
+                objectFit='cover'
+                src={url}
+                alt={url}
+                width='322px'
+                height='322px'
+              />
+            </Center>
+            <Center>
+              <Heading my={4}  fontSize='xl'>BAYC #{number}</Heading>
+            </Center>
+            <Stack
+              divider={<Divider borderColor='gray.200' />}
+              spacing={4}
+              align='stretch'>
+              <Popover>
+                  <PopoverTrigger>
+                      <Button type="button" w='100%' style={{
+                          backgroundColor: '#a3c139'
+                        }}>
+                          Create Auction...
+                      </Button>
+                  </PopoverTrigger>
+                  <PopoverContent bg={'#fef1df;'}>
+                      <PopoverBody border='0'
+                          display='flex'
+                          alignItems='center'
+                          justifyContent='space-between'
+                          pb={4}>
+                          <Box>Initial price:</Box>
+                          <NumberInput maxW='100px' mr='2rem' defaultValue={initialValue} onChange={(val) => setInitialValue(+val)}>
+                              <NumberInputField />
+                              <NumberInputStepper>
+                              <NumberIncrementStepper />
+                              <NumberDecrementStepper />
+                              </NumberInputStepper>
+                          </NumberInput>
+                      </PopoverBody>
+                      <PopoverBody border='0'
+                          display='flex'
+                          alignItems='center'
+                          justifyContent='space-between'
+                          pb={4}>
+                          <Box>Bidding period in minutes:</Box>
+                          <NumberInput maxW='100px' mr='2rem' defaultValue={increaseValue} onChange={(val) => setIncreaseValue(+val)}>
+                              <NumberInputField />
+                              <NumberInputStepper>
+                              <NumberIncrementStepper />
+                              <NumberDecrementStepper />
+                              </NumberInputStepper>
+                          </NumberInput>
+                      </PopoverBody>
+                      <PopoverBody border='0'
+                          display='flex'
+                          alignItems='center'
+                          justifyContent='space-between'
+                          pb={4}>
+                          <Box>Bid cost:</Box>
+                          <NumberInput maxW='100px' mr='2rem' defaultValue={costValue} onChange={(val) => setCostValue(+val)}>
+                              <NumberInputField />
+                              <NumberInputStepper>
+                              <NumberIncrementStepper />
+                              <NumberDecrementStepper />
+                              </NumberInputStepper>
+                          </NumberInput>
+                      </PopoverBody>
+                      <PopoverBody border='0'
+                          display='flex'
+                          alignItems='center'
+                          alignContent={'right'}
+                          justifyContent='flex-end'
+                          pb={4}>
+                            <Button disabled={buttonDisabled} colorScheme='blue' onClick={createAuction} style={{
+                  backgroundColor: '#a3c139',
+                  color: 'black'
+                }}>Start auction</Button>
+                      </PopoverBody>
+                      <PopoverFooter>All auctions will have a 10 minute bidding period</PopoverFooter>
+                  </PopoverContent>
+              </Popover>
+            </Stack>
+          </Box>
+    )
+  }
