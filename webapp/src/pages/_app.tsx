00ac2b2b075fa8949a09887d04a1d77cba656da3
Orange <osdev123@proton.me>
2023-08-04 21:24:40 +0100

    Created startup page

---
+++ webapp/src/pages/_app.tsx
@@ -0,0 +1,17 @@
+// src/pages/_app.tsx
+import { ChakraProvider } from '@chakra-ui/react'
+import type { AppProps } from 'next/app'
+import { Layout } from 'components/layout'
+import "../styles/App.css"
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+      <ChakraProvider>
+        <Layout>
+        <Component {...pageProps}/>
+        </Layout>
+      </ChakraProvider>
+  )
+}
+
+export default MyApp
