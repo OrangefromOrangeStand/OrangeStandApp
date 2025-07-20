ae9fded55365e8cba1105c51910df00b634be543
Orange <osdev123@proton.me>
2025-07-20 20:33:18 +0100

    Updated OrangeStandTicket tests

--- backend/test/orangeStandTicket.ts
+++ backend/test/orangeStandTicket.ts
@@ -2,72 +2,143 @@ import { ethers } from 'hardhat';
 import { expect } from 'chai';
 import { Contract } from 'ethers';
 
-describe('OrangeStandTicket tests', function () {
-    
+describe('OrangeStandTicket', function () {
     let orangeStandTicket: Contract;
+    let spentTicket: Contract;
     let userContract: Contract;
+    let owner: any;
+    let addr1: any;
+    let addr2: any;
+    let addr3: any;
 
-    describe('OrangeStandTicket', function () {
-        const contractAddress = process.env.CONTRACT_ADDRESS;
+    beforeEach(async function () {
+        [owner, addr1, addr2, addr3] = await ethers.getSigners();
+        const CollectionErc20 = await ethers.getContractFactory('CollectionErc20');
+        userContract = await CollectionErc20.deploy("usdTCollection", "USDT", 8);
+        const OrangeStandSpentTicket = await ethers.getContractFactory('OrangeStandSpentTicket');
+        spentTicket = await OrangeStandSpentTicket.deploy(await userContract.getAddress());
+        const OrangeStandTicket = await ethers.getContractFactory('OrangeStandTicket');
+        orangeStandTicket = await OrangeStandTicket.deploy(await userContract.getAddress(), await spentTicket.getAddress());
+    });
 
-        if (contractAddress) {
-            it('Should connect to external contract', async function () {
-                orangeStandTicket = await ethers.getContractAt('OrangeStandTicket', contractAddress);
-                console.log('Connected to external contract', orangeStandTicket.address);
-            });
-        } else {
-            it('Should deploy OrangeStandTicket', async function () {
-                const OrangeStandSpentTicket = await ethers.getContractFactory('OrangeStandSpentTicket');
-                const CollectionErc20 = await ethers.getContractFactory('CollectionErc20');
-                userContract = await CollectionErc20.deploy("usdTCollection", "USDT");
-                const orangeStandSpentTicket = await OrangeStandSpentTicket.deploy(await userContract.getAddress());
-                const OrangeStandTicket = await ethers.getContractFactory('OrangeStandTicket');
-                orangeStandTicket = await OrangeStandTicket.deploy(await userContract.getAddress(), await orangeStandSpentTicket.getAddress());
-            });
-        }
-
-        describe('mint()', function () {
-            it('Should mint one ticket', async function () {
-                const [owner, addr1] = await ethers.getSigners();
-                var initialBalance = await orangeStandTicket.balanceOf(addr1.address);
-                let bidPrice = 1;
-                await userContract.mint(addr1.address, bidPrice);
-                await userContract.connect(addr1).approve(await orangeStandTicket.getAddress(), bidPrice);
-                await orangeStandTicket.mint(addr1.address, bidPrice);
-                var balanceAfterMint = await orangeStandTicket.balanceOf(addr1.address);
-                expect(initialBalance).to.equal(0);
-                expect(balanceAfterMint).to.equal(1);
-            })
-
-            it('Should mint no tickets', async function () {
-                const [owner, addr1, addr2] = await ethers.getSigners();
-                var initialBalance = await orangeStandTicket.balanceOf(addr2.address);
-                await orangeStandTicket.mint(addr2.address, 0);
-                var balanceAfterMint = await orangeStandTicket.balanceOf(addr2.address);
-                expect(initialBalance).to.equal(0);
-                expect(balanceAfterMint).to.equal(0);
-            })
-
-            it('Only owner can mint tokens', async function () {
-                const [_, addr1, nonOwnerCaller] = await ethers.getSigners();
-                await expect(orangeStandTicket.connect(nonOwnerCaller).mint(addr1.address, 1)).to.be.reverted;
-            })
+    describe('Deployment', function () {
+        it('Should set correct owner', async function () {
+            expect(await orangeStandTicket.owner()).to.equal(owner.address);
         });
 
-        describe('burn()', function () {
-            it('Should burn all tickets', async function () {
-                const [owner, addr1, addr2, addr3] = await ethers.getSigners();
-                await orangeStandTicket.addBurner(owner.address);
-                let bidPrice = 1;
-                await userContract.mint(addr3.address, bidPrice);
-                await userContract.connect(addr3).approve(await orangeStandTicket.getAddress(), bidPrice);
-                await orangeStandTicket.mint(addr3.address, 1);
-                var balanceAfterMint = await orangeStandTicket.balanceOf(addr3.address);
-                await orangeStandTicket.burn(addr3.address, 1);
-                var balanceAfterBurn = await orangeStandTicket.balanceOf(addr3.address);
-                expect(balanceAfterMint).to.equal(1);
-                expect(balanceAfterBurn).to.equal(0);
-            })
+        it('Should set correct user contract and spent ticket addresses', async function () {
+            // These are private, but we can check via constructor args
+            // Or test via decimals() override
+            expect(await orangeStandTicket.decimals()).to.equal(await userContract.decimals());
+        });
+    });
+
+    describe('Roles', function () {
+        it('Owner can add burner', async function () {
+            await orangeStandTicket.addBurner(addr1.address);
+            expect(await orangeStandTicket.hasRole(await orangeStandTicket.BURNER_ROLE(), addr1.address)).to.be.true;
+        });
+
+        it('Non-owner cannot add burner', async function () {
+            await expect(orangeStandTicket.connect(addr2).addBurner(addr3.address)).to.be.reverted;
+        });
+    });
+
+    describe('mint()', function () {
+        it('Should mint tickets and emit event', async function () {
+            let bidPrice = 5;
+            await userContract.mint(addr1.address, bidPrice);
+            await userContract.connect(addr1).approve(await orangeStandTicket.getAddress(), bidPrice);
+
+            await expect(orangeStandTicket.mint(addr1.address, bidPrice))
+                .to.emit(orangeStandTicket, "TicketIssued")
+                .withArgs(addr1.address, bidPrice, await ethers.provider.getBlockNumber() + 1);
+
+            expect(await orangeStandTicket.balanceOf(addr1.address)).to.equal(bidPrice);
+        });
+
+        it('Should mint zero tickets and emit event', async function () {
+            await expect(orangeStandTicket.mint(addr2.address, 0))
+                .to.emit(orangeStandTicket, "TicketIssued")
+                .withArgs(addr2.address, 0, await ethers.provider.getBlockNumber() + 1);
+
+            expect(await orangeStandTicket.balanceOf(addr2.address)).to.equal(0);
+        });
+
+        it('Only owner can mint tokens', async function () {
+            await expect(orangeStandTicket.connect(addr2).mint(addr1.address, 1)).to.be.reverted;
+        });
+
+        it('Should transfer payment to spentTicket contract', async function () {
+            let bidPrice = 3;
+            await userContract.mint(addr1.address, bidPrice);
+            await userContract.connect(addr1).approve(await orangeStandTicket.getAddress(), bidPrice);
+
+            await orangeStandTicket.mint(addr1.address, bidPrice);
+
+            expect(await userContract.balanceOf(await spentTicket.getAddress())).to.equal(bidPrice);
+        });
+    });
+
+    describe('burn()', function () {
+        it('Should burn tickets if caller has burner role', async function () {
+            let bidPrice = 2;
+            await orangeStandTicket.addBurner(owner.address);
+            await userContract.mint(addr3.address, bidPrice);
+            await userContract.connect(addr3).approve(await orangeStandTicket.getAddress(), bidPrice);
+            await orangeStandTicket.mint(addr3.address, bidPrice);
+
+            expect(await orangeStandTicket.balanceOf(addr3.address)).to.equal(bidPrice);
+
+            await orangeStandTicket.burn(addr3.address, bidPrice);
+
+            expect(await orangeStandTicket.balanceOf(addr3.address)).to.equal(0);
+        });
+
+        it('Should revert burn if caller does not have burner role', async function () {
+            let bidPrice = 1;
+            await userContract.mint(addr2.address, bidPrice);
+            await userContract.connect(addr2).approve(await orangeStandTicket.getAddress(), bidPrice);
+            await orangeStandTicket.mint(addr2.address, bidPrice);
+
+            await expect(orangeStandTicket.connect(addr2).burn(addr2.address, bidPrice)).to.be.revertedWith("Caller is not a burner");
+        });
+    });
+
+    describe('decimals()', function () {
+        it('Should return decimals from user contract', async function () {
+            expect(await orangeStandTicket.decimals()).to.equal(await userContract.decimals());
+        });
+    });
+
+    describe('ERC20 Standard Compliance', function () {
+        it('Should return name and symbol', async function () {
+            expect(await orangeStandTicket.name()).to.equal('OrangeStandTicket');
+            expect(await orangeStandTicket.symbol()).to.equal('OSTI');
+        });
+
+        it('Should allow transfer between accounts', async function () {
+            let bidPrice = 4;
+            await userContract.mint(addr1.address, bidPrice);
+            await userContract.connect(addr1).approve(await orangeStandTicket.getAddress(), bidPrice);
+            await orangeStandTicket.mint(addr1.address, bidPrice);
+
+            await orangeStandTicket.connect(addr1).transfer(addr2.address, 2);
+            expect(await orangeStandTicket.balanceOf(addr1.address)).to.equal(2);
+            expect(await orangeStandTicket.balanceOf(addr2.address)).to.equal(2);
+        });
+
+        it('Should allow approve and transferFrom', async function () {
+            let bidPrice = 6;
+            await userContract.mint(addr1.address, bidPrice);
+            await userContract.connect(addr1).approve(await orangeStandTicket.getAddress(), bidPrice);
+            await orangeStandTicket.mint(addr1.address, bidPrice);
+
+            await orangeStandTicket.connect(addr1).approve(addr2.address, 3);
+            await orangeStandTicket.connect(addr2).transferFrom(addr1.address, addr3.address, 3);
+
+            expect(await orangeStandTicket.balanceOf(addr1.address)).to.equal(3);
+            expect(await orangeStandTicket.balanceOf(addr3.address)).to.equal(3);
         });
     });
 });
